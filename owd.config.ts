67a25ac925f113e2b7c6256d1a25ba07b6b52245
dxlliv <hello@dxlliv.dev>
2025-04-17 02:16:02 +0200

    refactor: Update using application entries

--- owd.config.ts
+++ owd.config.ts
@@ -1,34 +1,31 @@
 export default defineDesktopApp({
-  id: "org.owdproject.debug",
-  name: "Debug",
-  icon: "mdi:bug",
-  windows: {
-    main: {
-      component: () => import('./app/components/Window/WindowDebug.vue'),
-      name: "WindowDebug",
-      category: "system-tools",
-      title: "Debug",
-      icon: "mdi:bug",
-      pinned: true,
-      resizable: true,
-      overflow: true,
-      size: {
-        width: 400,
-        minHeight: 500
-      },
-      position: {
-        x: 400,
-        y: 240,
-        z: 0
-      },
-    }
-  },
-  commands: {
-    debug: (app) => {
-      app.openWindow("main")
-    }
-  },
-  onLaunch: (app) => {
-    app.openWindow('main')
-  }
+    id: "org.owdproject.debug",
+    title: "Debug",
+    category: "system-tools",
+    icon: "mdi:bug",
+    windows: {
+        main: {
+            component: () => import('./app/components/Window/WindowDebug.vue'),
+            resizable: true,
+            size: {
+                width: 400,
+                minHeight: 500
+            },
+            position: {
+                x: 400,
+                y: 240,
+                z: 0
+            },
+        }
+    },
+    entries: {
+        debug: {
+            command: 'debug'
+        }
+    },
+    commands: {
+        debug: (app) => {
+            app.openWindow("main")
+        }
+    },
 })
