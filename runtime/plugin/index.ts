12cb02c391d53aa46e377b45eab9ccfdcbd6645e
dxlliv <hello@dxlliv.dev>
2025-05-09 15:58:54 +0200

    refactor: Rename utils files

--- runtime/plugin/index.ts
+++ runtime/plugin/index.ts
@@ -1,5 +1,5 @@
 import {defineNuxtPlugin} from "nuxt/app"
-import {defineDesktopApp} from "@owdproject/core/runtime/utils/utilsDesktop"
+import {defineDesktopApp} from "@owdproject/core/runtime/utils/utilDesktop"
 import configAppDebug from '../../owd.config'
 
 export default defineNuxtPlugin({
