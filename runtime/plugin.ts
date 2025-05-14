10e16e7f93e0050ff603c7a12ad2e271ecf74be7
dxlliv <hello@dxlliv.dev>
2025-05-14 18:46:14 +0200

    refactor: Rename owd.config.ts in app.config.ts

--- runtime/plugin.ts
+++ runtime/plugin.ts
@@ -1,6 +1,6 @@
 import {defineNuxtPlugin} from "nuxt/app"
 import {defineDesktopApp} from "@owdproject/core/runtime/utils/utilDesktop"
-import configAppDebug from './owd.config'
+import configAppDebug from './app.config'
 
 export default defineNuxtPlugin({
     parallel: true,
